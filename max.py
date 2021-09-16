35e005a8235ddf1191021a3637676786d3d36d3b
Mijael26 <90813944+Mijael26@users.noreply.github.com>
2021-09-16 00:18:50 -0300

    Add files via upload

---
+++ max.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cont=1
+while cont <=5:
+       n=int(input("ingrese numero: "))
+       if cont==1:
+             max=n
+       else:
+           if n>max:
+
+
+               max=n
+
+       cont = cont + 1
+
+print(f"valor maximo es :{max}")
+
+
+
+
+
+
